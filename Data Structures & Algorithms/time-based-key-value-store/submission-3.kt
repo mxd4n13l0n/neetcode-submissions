b33741a6694097a96f3ac75823631a86d32aa5d1
Daniel Gallegos <mxdanielon@gmail.com>
2026-09-03 22:55:13 -0600

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.kt
@@ -0,0 +1,41 @@
+class TimeMap() {
+    val values = mutableMapOf<String, MutableList<Pair<Int, String>>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        val pair = Pair(timestamp, value)
+        if (values.contains(key)) {
+            values[key]!!.add(pair)
+        } else {
+            values[key] = mutableListOf<Pair<Int, String>>(pair)
+        }
+    }
+
+    fun get(key: String, timestamp: Int): String {
+        if (values.contains(key)) {
+            return searchLargestValueWithTimestamp(values[key]!!, timestamp)
+        } else {
+            return ""
+        }
+    }
+
+    private fun searchLargestValueWithTimestamp(searchList: MutableList<Pair<Int, String>>, timestamp: Int): String {
+        var l = 0
+        var r = searchList.lastIndex
+        if (searchList[l].first > timestamp) {
+            return ""
+        }
+        while (l <= r) {
+            val m = (l + r) / 2
+            val current = searchList[m]
+            if (current.first == timestamp) {
+                return current.second
+            }
+            if (current.first > timestamp) {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        return searchList[r].second
+    }
+}
